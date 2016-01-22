44eb6e9e28e4445cbc541f5fdc56f5b8dd8d23ac
edithcapistrang <edithcapistrang@gmail.com>
2016-01-21 22:30:18 -0600

    Generation MVC project, database connection, include bootstrap and Home view to show a data from the table "Alumno".

--- WebSite/WebSite/Controllers/HomeController.cs
+++ WebSite/WebSite/Controllers/HomeController.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Model;
 namespace WebSite.Controllers
 {
     public class HomeController : Controller
     {
+        //generate an object alumno from Alumno Class
+        private Alumno alumno = new Alumno();
         //
         // GET: /Home/
 
         public ActionResult Index()
-        {
-            return View();
+        {            
+            return View(alumno.Listar());
         }
 
     }
